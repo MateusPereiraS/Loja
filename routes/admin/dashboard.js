b31bc411d775b4d52ecc01b5fb5994c4a6952fe9
Andre <p4n1c0021@gmail.com>
2021-03-16 00:16:23 -0300

    ok

--- routes/admin/dashboard.js
+++ routes/admin/dashboard.js
@@ -5,7 +5,7 @@ const mongoose = require("mongoose")
 require("../../models/Usuario")
 const Usuario = mongoose.model("usuarios")
 
-router.get("/dashboard", (req, res) => {
+router.get("/", (req, res) => {
     res.render("admin/dashboard")
 })
 
